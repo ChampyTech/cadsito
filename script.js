9b61f0938fa08899f5e20ef0b8f7a644ed19f098
ChampyTech <66367029+ChampyTech@users.noreply.github.com>
2025-08-25 12:36:30 +0200

    Remove Table of Contents with icon

--- script.js
+++ script.js
@@ -3,9 +3,3 @@ function associazione() {
     if (confirm(messaggio)) alert('Sei entrato con successo nella nostra associazione! Sono 72€ + IVA.');
     else alert('Ci dispiace che tu non ci voglia vendere la tua anima.');
 }
-
-function linksButton() {
-    var x = document.getElementById('links');
-    if (x.style.display === 'block') x.style.display = 'none'
-    else x.style.display = 'block';
-}
