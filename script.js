d42c816bbe62bcc2e8a9a76a5ca4fa368633ccf7
ChampyTech <66367029+ChampyTech@users.noreply.github.com>
2025-09-10 15:26:38 +0200

    Remove script.js & edit association

--- script.js
+++
@@ -1,5 +0,0 @@
-function associazione() {
-    let messaggio = 'Sei sicuro di voler entrare a far parte della nostra associazione? Non puoi tornare indietro.';
-    if (confirm(messaggio)) alert('Sei entrato con successo nella nostra associazione! Sono 72€ + IVA.');
-    else alert('Ci dispiace che tu non ci voglia vendere la tua anima.');
-}
